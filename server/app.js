ecc56dcf19919809fc4ff76253d22566deff22de
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2022-11-30 13:02:27 +0300

    connected MongoDB

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const config = require("config");
+const chalk = require("chalk");
+
+const app = express();
+
+const PORT = config.get("port") ?? 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"));
+    console.log(chalk.green(`MongoDB connected`));
+    app.listen(PORT, () =>
+      console.log(chalk.green(`Server has been started on port ${PORT}`))
+    );
+  } catch (error) {
+    console.log(chalk.red(e.message));
+    process.exit(1);
+  }
+}
+
+start();
