95d6f3a284ae0fa352f15b6498944a77e6b4d66e
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2022-12-03 11:16:23 +0300

    lesson 7 finished

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const User = require("../models/User");
+const auth = require("../middleware/auth.middleware");
 const router = express.Router({ mergeParams: true });
+
+router.patch("/:userId", auth, async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    if (userId === req.user._id) {
+      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+        new: true,
+      });
+
+      res.send(updatedUser);
+    } else {
+      res.status(401).json({ message: "Unauthorazed" });
+    }
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: "На сервере произошла ошибка. Попробуйте позже." });
+  }
+});
 
 router.get("/", async (req, res) => {
   try {
